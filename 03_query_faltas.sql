e9e676caab5726790c37f78185ba8a74bb1e16dd
rafaelSanchez4330 <rafaelsanchez9924@gmail.com>
2025-09-10 00:16:49 -0600

    Update 03_query_faltas.sql

--- 03_query_faltas.sql
+++ 03_query_faltas.sql
@@ -1,6 +1,13 @@
-    SELECT g.periodo, g.seccion, g.nombre_grupo,
-           COUNT(*) FILTER (WHERE a.presente = FALSE) AS total_faltas
-    FROM asistencia a
-    JOIN grupos g ON a.periodo = g.periodo AND a.seccion = g.seccion
-    GROUP BY g.periodo, g.seccion, g.nombre_grupo
-    ORDER BY g.seccion;
+SELECT 
+    g.periodo, 
+    g.seccion, 
+    m.nombre AS nombre_materia,
+    COUNT(*) FILTER (WHERE a.presente = FALSE) AS total_faltas
+FROM asistencia a
+JOIN grupo g 
+    ON a.periodo = g.periodo 
+   AND a.idGrupo = g.seccion
+JOIN materias m
+    ON g.idMateria = m.idMateria
+GROUP BY g.periodo, g.seccion, m.nombre
+ORDER BY g.seccion;
